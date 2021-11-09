61e8eafd1475f94484432e04128679f82b548d20
justinaoki <justinaoki26@gmail.com>
2021-11-08 16:03:19 -0800

    CLIclick implemented with MQTT

--- publisherTest.py
+++ publisherTest.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-import numpy as np
+# import numpy as np
 
 # 0. define callbacks - functions that run when events happen.
 # The callback for when the client receives a CONNACK response from the server.
@@ -39,8 +39,7 @@ client.loop_start()
 # 4. use subscribe() to subscribe to a topic and receive messages.
 # 5. use publish() to publish messages to the broker.
 # payload must be a string, bytearray, int, float or None.
-for i in range(10):
-  client.publish('ece180d/test', float(np.random.random(1)), qos=1)
+client.publish('ece180d/test', "Run CLIclick", qos=1)
 # 6. use disconnect() to disconnect from the broker.
 client.loop_stop()
 client.disconnect()
